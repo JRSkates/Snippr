3f03e696c4ad78bba51ebaa5a11037ccfc228935
JRSkates <skatesproduction@gmail.com>
2025-01-15 09:43:20 +0000

    json seed data read by API calls, routes tested

--- models/snippets.js
+++ models/snippets.js
@@ -1,31 +1,35 @@
-let snippets = [
-    { id: 1, language: 'JavaScript', code: 'console.log("Hello, world!");' },
-    { id: 2, language: 'Python', code: 'print("Hello, world!")' },
-  ];
-  
-  let nextId = 3;
-  
-  // Get all snippets
-  function getAllSnippets() {
-    return snippets;
-  }
-  
-  // Get snippet by ID
-  function getSnippetById(id) {
-    return snippets.find(snippet => snippet.id === id);
-  }
-  
-  // Add a new snippet
-  function addSnippet(language, code) {
-    const newSnippet = { id: nextId++, language, code };
-    snippets.push(newSnippet);
-    return newSnippet;
-  }
-  
-  // Get snippets by language
-  function getSnippetsByLanguage(lang) {
-    return snippets.filter(snippet => snippet.language.toLowerCase() === lang.toLowerCase());
-  }
-  
-  module.exports = { getAllSnippets, getSnippetById, addSnippet, getSnippetsByLanguage };
-  
+const fs = require('fs');
+const path = require('path');
+
+// Load seed data from the JSON file
+const filePath = path.join(__dirname, '../data/snippets.json');
+let snippets = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+let nextId = snippets.length + 1;
+
+// Get all snippets
+function getAllSnippets() {
+  return snippets;
+}
+
+// Get snippet by ID
+function getSnippetById(id) {
+  return snippets.find(snippet => snippet.id === id);
+}
+
+// Add a new snippet
+function addSnippet(language, code) {
+  const newSnippet = { id: nextId++, language, code };
+  snippets.push(newSnippet);
+
+  // Persist the new snippet to the JSON file
+  fs.writeFileSync(filePath, JSON.stringify(snippets, null, 2));
+  return newSnippet;
+}
+
+// Get snippets by language
+function getSnippetsByLanguage(lang) {
+  return snippets.filter(snippet => snippet.language.toLowerCase() === lang.toLowerCase());
+}
+
+module.exports = { getAllSnippets, getSnippetById, addSnippet, getSnippetsByLanguage };
